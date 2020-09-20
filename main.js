7af6ecbc9d229ffd92edee56d4876a1ea727f1d8
gbolahanprojects <gbolahanoduyemi1@gmail.com>
2020-09-20 12:58:29 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const menuToggle = selectElement('.menu-toggle');
+const navList = selectElement('.nav__list');
+const header = selectElement('header');
+
+function selectElement(element) {
+  return document.querySelector(element);
+}
+
+function loadEventListeners() {
+  menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('toggle');
+    navList.classList.toggle('nav-active');
+    
+  });
+}
+
+loadEventListeners();
+
+gsap.from('.intro__img', { opacity: 0, duration: 1, y: -50 });
+gsap.from('.intro__content h1', { opacity: 0, duration: 1, x: -50 });
+gsap.from('.intro__content p', { opacity: 0, duration: 1, x: 50 });
+gsap.from('.intro a', { opacity: 0, duration: 1, y: 50 });
+
+gsap.to(
+  ".text-medium",
+
+  0.7,
+  { x: "-40",  ease: "power2.inOut",
+    // Reverse the animation back to start
+    yoyo: true,
+    // Delay the next item in seconds
+    stagger: 0.08,
+    // Infinite repeat
+    repeat: -1
+  }
+);
+
